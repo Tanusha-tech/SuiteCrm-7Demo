24daae58f272084cab70a5cdaa0cf270a18230c9
Lyamkina Tatyana <lana78l96@gmail.com>
2026-06-11 14:56:25 +0300

    added headless options

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -38,6 +38,7 @@ public class BaseTest {
         options.addArguments("--disable-notifications");
         options.addArguments("--disable-popup-blocking");
         options.addArguments("--disable-infobars");
+        options.addArguments("--headless");
 
         driver = new ChromeDriver(options);
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
