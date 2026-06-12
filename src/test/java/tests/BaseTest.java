8b2013f29acdfffcca0389fcd0362b7cf777e7a3
Lyamkina Tatyana <lana78l96@gmail.com>
2026-06-12 09:37:13 +0300

    add crossBrowser.xml

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -4,9 +4,13 @@ import io.qameta.allure.Description;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
 import pages.NewAccountPage;
 import pages.NewContactPage;
 import step.LoginStep;
@@ -24,23 +28,25 @@ public class BaseTest {
 
     @BeforeMethod(alwaysRun = true)
     @Description("Настройка браузера")
-    public void setUp() {
-
-        ChromeOptions options = new ChromeOptions();
-
-        HashMap<String, Object> chromePrefs = new HashMap<>();
-        chromePrefs.put("credentials_enable_service", false);
-        chromePrefs.put("profile.password_manager_enabled", false);
-
-        options.setExperimentalOption("prefs", chromePrefs);
-
-        options.addArguments("--incognito");
-        options.addArguments("--disable-notifications");
-        options.addArguments("--disable-popup-blocking");
-        options.addArguments("--disable-infobars");
-        options.addArguments("--headless");
-
-        driver = new ChromeDriver(options);
+    public void setUp(@Optional("chrome") String browser, ITestContext iTestContext) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            ChromeOptions options = new ChromeOptions();
+            HashMap<String, Object> chromePrefs = new HashMap<>();
+            chromePrefs.put("credentials_enable_service", false);
+            chromePrefs.put("profile.password_manager_enabled", false);
+            options.setExperimentalOption("prefs", chromePrefs);
+            options.addArguments("--incognito");
+            options.addArguments("--disable-notification");
+            options.addArguments("--disable-popup_blocking");
+            options.addArguments("--disable-infobars");
+            options.addArguments("--headless");
+            driver = new ChromeDriver(options);
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            FirefoxOptions options = new FirefoxOptions();
+            options.addArguments("--headless");
+            driver = new FirefoxDriver();
+            driver.manage().window().maximize();
+        }
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         newAccountPage = new NewAccountPage(driver);
         newContactPage = new NewContactPage(driver);
