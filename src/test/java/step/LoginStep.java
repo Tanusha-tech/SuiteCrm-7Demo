9815d6fb3da095cdb12e4a760e565e37cda46c37
Lyamkina Tatyana <lana78l96@gmail.com>
2026-06-12 11:40:42 +0300

    fix step

--- src/test/java/step/LoginStep.java
+++ src/test/java/step/LoginStep.java
@@ -1,5 +1,6 @@
 package step;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import pages.LoginPage;
 
@@ -12,6 +13,7 @@ public class LoginStep {
         this.driver = driver;
         loginPage = new LoginPage(driver);
     }
+    @Step("Авторизация пользователя")
     public void auth(String user, String password) {
         loginPage.open()
                 .login(user, password);
